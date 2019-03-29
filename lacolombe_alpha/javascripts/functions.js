ea1ccaa111781bf2cd14b0da70fd1cb40a90a951
otelhan <otelhan@gmail.com>
2019-03-29 13:02:34 -0400

    lacolombe_alpha folder change

---
+++ lacolombe_alpha/javascripts/functions.js
@@ -0,0 +1,80 @@
+    window.onload = function(){
+
+    document.getElementById("togBtn").checked=false; 
+
+   }
+
+
+    function parseInstructions(m){
+
+    	var experimentId = m.message.eon.run.syringe_id;
+    	var date = m.message.eon.run.date;
+    	var organismMedia = m.message.eon.run.instructions.organism_media;
+    	var volume = m.message.eon.run.instructions.volume+ ' ml';
+    	var targetTemperature = m.message.eon.run.instructions.target_temperature + ' C';
+    	var duration = ((m.message.eon.run.instructions.duration) / 3600) + ' hrs' ;
+    	//var obj2 = JSON.parse(obj.message);
+    	//var obj3 = JSON.parse(obj2.eon);
+    	//var temp = JSON.parse(obj3.Temperature);
+        //document.getElementById("instructions").innerHTML =  "<span style=\"color:#355ea3\">" + "Syringe ID" + "</span>" + '&nbsp &nbsp &nbsp &nbsp'+ "<span style=\"color:black\">" + data + "</span>"; ; 
+        
+        document.getElementById("instructions").innerHTML = 
+        '<br> <span class="label syringe_id">Experiment ID </span> <span class="label other">' + experimentId +  
+        '</span><br><br> <span class="label date">Date</span><span class="label other">'+ date +
+        '</span><br><br><span class="label organism_media">Organism + Media</span><span class="label other"> <i>'+ organismMedia +
+        '</i> </span><br><br><span class="label volume">Volume</span><span class="label other">'+ volume +
+        '</span><br><br><span class="label temperature">Temperature</span><span class="label other">'+ targetTemperature +
+        '</span><br><br><span class="label duration">Duration</span><span class="label other">'+ duration + 
+        '</span><br><br>'
+
+
+        //document.getElementsByClassName(".switch").style.opacity = "1"; 
+        document.getElementById("button_label").style.opacity = "1"; 
+        document.getElementById("togBtn").checked=true; 
+
+        var specFreq = m.message.eon.run.instructions.spec_interval;
+        var agitationMode = m.message.eon.run.instructions.agitation_mode; 
+        var LidStatus = m.message.eon.run.instructions.lid ;
+        var LEDStatus = m.message.eon.run.instructions.led ;
+        var UVStatus = m.message.eon.run.instructions.uv ;
+
+        document.getElementById("devicetatus").innerHTML = 
+        '<span class="label status">Spec Frequency:</span><span class="label status">'+ specFreq +
+        '</span><br><span class="label status">Agitation Mode:</span><span class="label status">'+ agitationMode +
+        '</span><br><span class="label status">Lid:</span><span class="label status">'+ LidStatus +
+        '</span><br><span class="label status">LED:</span><span class="label status">'+ LEDStatus +
+        '</span><br><span class="label status">UV:</span><span class="label status">'+ UVStatus +
+        '</span><br>'
+
+        var currentTemp = m.message.eon.temperature ;
+        document.getElementById("currentTemperature").innerHTML = 
+        '<span class="label status">Temperature:</span><span style="color:#ff9800">'+ currentTemp +
+        '</span><br>'
+
+
+
+    }
+
+    function UpdateButton(chkButton) {
+        if (chkButton.checked){
+            //console.log("checked");
+            pubnub.publish({
+                channel : 'fromdevice-red',
+                message : { device: 'start'},
+                callback : function(m){
+                    callbackonsole.log(m)
+                }
+            });
+        }
+        else{
+            pubnub.publish({
+                channel : 'fromdevice-red',
+                message : { device: 'stop'},
+                callback : function(m){
+                    callbackonsole.log(m)
+                }
+            });
+            //console.log("not checked");
+        }
+
+    }
---
+++ lacolombe_alpha/javascripts/visuals.js
@@ -0,0 +1,172 @@
+    
+    var pubnub = new PubNub({
+
+        subscribeKey: "sub-c-0b2aaa44-a779-11e6-be20-0619f8945a4f",
+        publishKey: "pub-c-722b1270-6c2b-423d-98f6-cb21c18a2265",
+        
+    });
+
+    //node-red: once per run
+    //node-red2: real-time array data
+    pubnub.subscribe({ channels: ['node-red', 'node-red2', 'fromdevice-red'] });
+
+    pubnub.addListener({
+
+        message: function (m) {
+          var channelName = m.channel;
+          console.log('message came in: ', m);
+            if ( channelName ==='node-red') {
+              parseInstructions(m);
+            }
+        }
+        });  
+
+  //var channel = 'node-red';
+
+  eon.chart({
+    channels: ['node-red2'],
+    xType: 'custom',
+    xId: 'spectra_timestamp',
+    
+    generate: {
+      bindto: '#spectra',
+      point: {
+        r: 1
+      },
+      data: {
+        type: 'spline',
+        colors: {
+          spectra: 'lightblue'
+        }
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%H:%m:%S',
+            fit: false
+          },
+          label: {
+            text: 'Spectral distribution over time'
+          }
+        },
+        y: {
+          label: {
+            text: 'Wavelengths',
+            position: 'outer-middle'
+          },
+          tick: {
+            format: function (d) {
+              var df = Number( d3.format('.2f')(d) );
+              return df;
+            }
+          }
+        }
+      }
+    },
+    pubnub: pubnub,
+    limit: 288,
+  });
+
+  //the temperature
+
+  eon.chart({
+    channels: ['node-red'],
+    generate: {
+      bindto: '#temperature', //was #od
+      point: {
+        r: 1
+      },
+      data: {
+        type: 'area-spline',
+        colors: {
+          temperature: 'deeppink'
+        }
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%H:%m:%S',
+            fit: false
+          },
+          label: {
+            text: 'Temperature over time', //Optical density over time
+          }
+        },
+        y: {
+          label: {
+            text: 'Celcius', //Absorbance
+            position: 'outer-middle'
+          },
+          tick: {
+            format: function (d) {
+              var df = Number( d3.format('.2f')(d) );
+              return df;
+            }
+          }
+        }
+      }
+    },
+    pubnub: pubnub,
+    limit: 2,
+
+    transform: function(m) {
+      return { eon: {
+        temperature: m.eon.temperature
+      }}
+    }
+  });
+
+
+  eon.chart({
+    channels: ['node-red'],
+    
+    oninit: function()
+    {
+        d3
+          .select(el)
+          .select("svg")
+          .append("text")
+          .attr("x", "50%" )
+          .attr("y", "100%")
+          .style("text-anchor", "middle")
+          .text("Time Elapsed");  
+    },
+
+    generate: {
+      bindto: '#time',
+      data: {
+        type:'gauge'
+      },
+      gauge: {
+        label: {
+            format: function(value, ratio) {
+                return value;
+            },
+            show: false // to turn off the min/max labels.
+        },
+        min: 0,
+        max: 86400
+      },
+      color: {
+        //pattern: ['#FF0000', '#F6C600', '#60B044'],
+        pattern:['#F6C600'],
+        //threshold: {
+        //  values: [341, 682]
+        //}
+      }
+    },
+        pubnub: pubnub,
+    limit: 30,
+    transform: function(m) {
+
+      var elapsed = m.eon.run.time_elapsed;
+      //var remainingMinutes = Math.floor(remainingSeconds / 60);
+    
+      return { eon: {
+        time_elapsed: elapsed}
+      }
+
+    }
+  });
