9cd295ee05e6064f032fee550467c300a4e176a9
otelhan <otelhan@gmail.com>
2019-03-31 21:43:55 -0400

    fixed the empty space problem

--- lacolombe01/javascripts/functions.js
+++ lacolombe01/javascripts/functions.js
@@ -44,10 +44,10 @@
     function loadNewImage(m){
 
         var image = document.images[2]; //2 corresponds to the clear image
-        //var downloadingImage = new Image();
-        //downloadingImage.onload = function(){
-        //    image.src = this.src;   
-        //};
+        var downloadingImage = new Image();
+        downloadingImage.onload = function(){
+            image.src = this.src;   
+        };
 
         //downloadingImage.src = "https://www.dropbox.com/s/b4ymnx6io3oh22p/Saturday%2030%20March%202019%2002%3A20%3A12PM.jpg?raw=1";
         
@@ -62,15 +62,10 @@
         
         var data = m.message[1] + "";
         var url = data.split("/")[2] + "";
-        var formatted_url = url.split(' ').join('%20');
-        //formatted_url = "https://biorealize.github.io/" + data;
-        //downloadingImage.src = formatted_url;
-        image = asyncImageLoader(formatted_url);
-        //console.log(formatted_url);
-
-        image.then( res => {
-            console.log(res)
-        })
+        //var formatted_url = url.split(' ').join('%20');
+        formatted_url = "https://biorealize.github.io/" + data;
+        downloadingImage.src = formatted_url;
+        console.log(formatted_url);
 
 
         //downloadingImage.src = "https://www.dropbox.com/s/b4ymnx6io3oh22p/Saturday%2030%20March%202019%2002%3A20%3A12PM.jpg?raw=1";
