57343e26ed954d13dc6407dcdbdc6ff558570bb3
jonny <megabulk@grundig9000.com>
2019-04-12 18:38:22 -0400

    moved Jquery to local for now. coded JS for controlling Submit form (but not submitting)

--- test_nfc/javascript/custom.js
+++ test_nfc/javascript/custom.js
@@ -1,8 +1,35 @@
-jQuery(document).ready(function($) {
-});
-//end jQuery document ready
+jQuery(document).ready(($) => {
+	$('input[name=media_type]').click((e) => {
+		var $this = $(e.currentTarget)
+		if ($this.is(':checked')) {
+			if ($this.attr('id') == 'media_type_solid') {
+				$('#peripheral_type_group').show()
+				$('#syringe_group').hide()
+				showPlate()
+				var table = $('.plate').filter((i, d) => {
+					return $(d).is(':visible')
+				})
+				table.find('input').eq(0).focus()
+				//$('.plate').is(':visible').find('input').eq(0).focus()
+			} else {
+				$('#peripheral_type_group, .form-row.plate').hide()
+				$('#syringe_group').show()
+			}
+		}
+	})
+	
+	
+	var showPlate = () => {
+		$('.form-row.plate').hide()
+		if ($('#media_type_solid').is(':checked')) {
+			var plate = $('#peripheral_type').val()
+			$('#' + plate).show()
+		}
+	}
+	$('#peripheral_type').change(showPlate).change()
+})
 
-function $c(t) {
-	console.log(t);
+var $c = (t) => {
+	console.log(t)
 }
 
