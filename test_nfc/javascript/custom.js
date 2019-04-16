705035a29ce8cff79e99bfa2e1c17ae687297a78
jonny <megabulk@grundig9000.com>
2019-04-16 13:51:04 -0400

    convert form to JS object on submit

--- test_nfc/javascript/custom.js
+++ test_nfc/javascript/custom.js
@@ -5,7 +5,7 @@ jQuery(document).ready(($) => {
 			if ($this.attr('id') == 'media_type_solid') {
 				$('#peripheral_type_group').show()
 				$('#syringe_group').hide()
-				showPlate()
+				switchPlate()
 				var table = $('.plate').filter((i, d) => {
 					return $(d).is(':visible')
 				})
@@ -19,17 +19,71 @@ jQuery(document).ready(($) => {
 	})
 	
 	
-	var showPlate = () => {
+	var switchPlate = () => {
 		$('.form-row.plate').hide()
 		if ($('#media_type_solid').is(':checked')) {
 			var plate = $('#peripheral_type').val()
 			$('#' + plate).show()
 		}
 	}
-	$('#peripheral_type').change(showPlate).change()
+	$('#peripheral_type').change(switchPlate).change()
+	
+	$('#submit_nfc_form').submit((e) => {
+		e.preventDefault()
+		var experiment = {}, nfc_obj = {}
+		var $this = $(e.currentTarget)
+		var fields = ['id', 'client', 'location', 'start_time', 'duration', 'target_temperature', 'imaging_frequency', ]
+		for (var i = 0; i < fields.length; i++) {
+			experiment[fields[i]] = $this.find('#' + fields[i]).val()
+		}
+		if ($this.find('input[name=media_type]:checked').val() == 'solid') {
+			experiment.peripheral_type = $this.find('#peripheral_type').val()
+			var load = {}
+			var $plate = $this.find('.form-row.plate:visible').find('table')
+			$c($plate)
+			$plate.find('tr').each((i, row) => {
+				var chr = String.fromCharCode(65 + i)
+				$(row).find('td').each((j, td) => {
+					load[chr + j] = $(td).find('input[type=text]').val()
+				})
+			})
+			experiment.load = load
+		} else {
+			experiment.syringe = $this.find('#syringe').val()
+		}
+		nfc_obj.experiment = experiment
+		$('#json_to_send').val(JSON.stringify(nfc_obj))
+		$c(nfc_obj)
+	})
 })
 
 var $c = (t) => {
 	console.log(t)
 }
 
+/*
+{ "experiment" :
+	{
+	id: 79E4464,
+	"client" : "Lacolombe",
+	"location" : 19102,
+	"start_time:" : Monday08April2019_09/37/28AM,
+	"duration" : 3600,
+	"peripheral_type" : "6_well_plate",
+	"load":
+	     [
+{
+		"A1" : organismA",
+		"A2" : organismB",
+		"A3" : organismC",
+		"B1" : organismD",
+		"B2" : organismE",
+		"B3" : organismF",  
+		}
+     ],
+	"target_temperature" : 37,
+	"imaging_frequency" : 3600,
+	"end_time": <filled when completed>
+	}
+}
+*/
