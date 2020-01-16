a21ff517e07030251ee01a33ee7b09e0a91de1b1
Orkan Telhan <info@biorealize.com>
2020-01-16 14:06:36 -0500

    visibility none

--- cons01/secure/42eba670fa2f3c4ef231e2312782a3cbe365ebf3/javascripts/functions.js
+++ cons01/secure/42eba670fa2f3c4ef231e2312782a3cbe365ebf3/javascripts/functions.js
@@ -516,7 +516,7 @@
         if (chkButton.checked){
             //console.log("checked");
             checkBox = document.getElementById("spin_box_id"); 
-            checkBox.style="display:visible";
+            checkBox.style="display:none";
             checkBox.opacity=0.3;
             checkBox.disabled = true;
             label = document.getElementById("spin_check_label");
