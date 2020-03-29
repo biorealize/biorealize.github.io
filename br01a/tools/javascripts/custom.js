0383c83930c0e7e97b28e0caa63239c082e781ab
Orkan Telhan <info@biorealize.com>
2020-03-29 14:45:08 -0400

    new stitch function added

--- br01a/tools/javascripts/custom.js
+++ br01a/tools/javascripts/custom.js
@@ -5,6 +5,8 @@ var pubnub = new PubNub({
 
 jQuery(document).ready(($) => {
 
+	loadDB();
+
 	pubnub.subscribe({
 		channels: ['NFC_TAG_CLIENT'] 
 	})
@@ -137,6 +139,27 @@ var getMeta = () => {
 	}
 }
 
+function loadDB(){
+
+	console.log("load DB Called");
+
+  const client = stitch.Stitch.initializeDefaultAppClient('experimentdesign-clijb');
+
+  const db = client.getServiceClient(stitch.RemoteMongoClient.factory, 'mongodb-atlas').db('BR_internal');
+
+  client.auth.loginWithCredential(new stitch.AnonymousCredential()).then(user =>
+    db.collection('Experiments').updateOne({owner_id: client.auth.user.id}, {$set:{number:42}}, {upsert:true})
+  ).then(() =>
+    db.collection('Experiments').find({owner_id: client.auth.user.id}, { limit: 100}).asArray()
+  ).then(docs => {
+      console.log("Found docs", docs)
+      console.log("[MongoDB Stitch] Connected to Stitch")
+  }).catch(err => {
+    console.error(err)
+  });
+
+
+}
 
 /*
 { "experiment" :
